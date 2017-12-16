cbf7113bc64d5b4302bdf0fdd148de04782da99f
saingledue <saingledue@outlook.com>
2017-12-16 09:10:56 -0600

    002Test

--- StarWars/StarWars/scripts/bountyHunter.js
+++ StarWars/StarWars/scripts/bountyHunter.js
@@ -1,4 +1,6 @@
-var weapons = [["Weapon", "Range", "Min Damage", "Max Damage", "Weight", "Attack Modifier", "Rate of Fire", "Cost"], ["Blaster Pistol", "Short", 1, 8, 5, 0, 2, 300], ["Blaster Rifle", "Medium", 1, 10, 8, 5, 2, 650], ["Blaster Sniper", "Long", 3, 18, 10, 10, 1, 1200], ["Vibro Blade", "Melee", 1, 6, 3, 0, 1, 50], ["Throwing Knife", "Short", 1, 4, 1, 0, 1, 25]];
+var weapons = [["Weapon", "Range", "Min Damage", "Max Damage", "Weight", "Attack Modifier", "Rate of Fire", "Cost"],
+    ["Blaster Pistol", "Short", 1, 8, 5, 0, 2, 300], ["Blaster Rifle", "Medium", 1, 10, 8, 5, 2, 650],
+    ["Blaster Sniper", "Long", 3, 18, 10, 10, 1, 1200], ["Vibro Blade", "Melee", 1, 6, 3, 0, 1, 50], ["Throwing Knife", "Short", 1, 4, 1, 0, 1, 25]];
 var armor = [["Light", 10, 1, 50, 120], ["Medium", 25, 3, 100, 375], ["Heavy", 75, 5, 150, 1200]];
 var items = [["To Be Determined - Item 1", 0], ["To Be Determined - Item 2", 0]];
 var bountyHunter = new BountyHunter(null, null, null, null, null, null, null, null, null);
